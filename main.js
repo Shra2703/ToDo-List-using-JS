3ee9969125d8348a634b9dbd2ea9e3b6a1ba25d5
Shraddha Goyal <shraddhagoyal800@gmail.com>
2022-09-18 01:07:50 +0530

    completed the logic of add task

--- main.js
+++ main.js
@@ -22,10 +22,35 @@ window.addEventListener('load', () => {
 
         const task_content_ele = document.createElement('div');
         task_content_ele.classList.add('content');
-        task_content_ele.innerText = task_val;
+        // task_content_ele.innerText = task_val;
 
+        const task_input_ele = document.createElement('input');
+        task_input_ele.classList.add('text');
+        task_input_ele.type = "text";
+        task_input_ele.value = task_val;
+        task_input_ele.setAttribute("readonly","readonly");
+
+    // adding content and content box to the div
         task_el.appendChild(task_content_ele);
+        task_content_ele.appendChild(task_input_ele);
 
+        const task_action_ele = document.createElement('div');
+        task_action_ele.classList.add('actions');
+
+        const button_edit = document.createElement('button');
+        button_edit.classList.add('edit');
+        button_edit.innerHTML = "Edit";
+
+        const button_delete = document.createElement('button');
+        button_delete.classList.add('delete');
+        button_delete.innerHTML = "Delete";
+
+
+        task_action_ele.appendChild(button_edit);
+        task_action_ele.appendChild(button_delete);
+
+        task_el.appendChild(task_action_ele);
+        
         list_el.appendChild(task_el);
 
 
